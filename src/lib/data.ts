7dbb74a90cdeb0bf7b60215639d5db7a37f731df
Andrew Goodridge <gandrew888@gmail.com>
2025-02-27 00:52:56 -0500

    Progress

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,17 @@
+const mockboxIcon = '/images/projectIcons/mockbox.png';
+const zineIcon = '/images/projectIcons/258zine.png';
+const fluxIcon = '/images/projectIcons/flux.png';
+const sketchesIcon = '/images/projectIcons/sketches.png';
+const coshufflerIcon = '/images/projectIcons/coshuffler.png';
+const pixelbloomIcon = '/images/projectIcons/pixelbloom.png';
+
+const portfolio2017Icon = '/images/portfolioFavicons/2017.png';
+const portfolio2018Icon = '/images/portfolioFavicons/2018.png';
+const portfolio2019Icon = '/images/portfolioFavicons/2019.png';
+const portfolio2020Icon = '/images/portfolioFavicons/2020.png';
+const portfolio2021Icon = '/images/portfolioFavicons/2021.png';
+const portfolio2022Icon = '/images/portfolioFavicons/2022.png';
+
 interface Social {
     platform: string;
     url?: string;
@@ -7,6 +21,7 @@ interface Social {
 interface Project {
     name: string;
     url?: string;
+    urlLabel?: string;
     icon: string;
     muxID: string;
 }
@@ -28,19 +43,19 @@ export const socials: Social[] = [
 ];
 
 export const projects: Project[] = [
-    {name: 'Mockbox', url: 'https://mockbox.app', icon: 'mockbox', muxID: 'CcyyWriJtLaBxiuqDSOWNL1hlY3IjfSMzx3riBJqKFg'},
-    {name: '258 Zine', url: 'https://258-zine.vercel.app', icon: '258zine', muxID: 'NIg02Zef7fuDcGFgAaP4dat00jvg0100jrUA235J7vJTF00E'},
-    {name: 'Various sketches', icon: 'sketches', muxID: 'UIL01hQXMCqMgr3GxVUJb35U027bDNvIKCDiP7ymVMCyc'},
-    {name: 'SCAD FLUX', icon: 'scadflux', url: 'https://scadflux.com', muxID: 'VzFZjv9JLKPjtssJQOzlUckeep9RgmgPIqkRxQIwA4I'},
-    {name: 'CoShuffler', icon: 'coshuffler', url: 'https://www.figma.com/community/plugin/974496433337884240/coshuffler', muxID: 'x7qlhrctWFtNyvBGLMNqsWDY01cjQsgN85T00re004phyw'},
-    {name: 'Pixel Bloom', icon: 'pixelbloom', muxID: 'WOAyqsQQ1tO02y8oZIrf7zPCXAuj6AfAdUI54GVuKXiA'},
+    {name: 'Mockbox', url: 'https://mockbox.app', urlLabel: 'mockbox.app', icon: mockboxIcon, muxID: 'CcyyWriJtLaBxiuqDSOWNL1hlY3IjfSMzx3riBJqKFg'},
+    {name: '258 Zine', url: 'https://258-zine.vercel.app', urlLabel: '258-zine.vercel.app', icon: zineIcon, muxID: 'NIg02Zef7fuDcGFgAaP4dat00jvg0100jrUA235J7vJTF00E'},
+    {name: 'Various sketches', icon: sketchesIcon, muxID: 'UIL01hQXMCqMgr3GxVUJb35U027bDNvIKCDiP7ymVMCyc'},
+    {name: 'SCAD FLUX', icon: fluxIcon, url: 'https://scadflux.com', urlLabel: 'scadflux.com', muxID: 'VzFZjv9JLKPjtssJQOzlUckeep9RgmgPIqkRxQIwA4I'},
+    {name: 'CoShuffler', icon: coshufflerIcon, url: 'https://www.figma.com/community/plugin/974496433337884240/coshuffler', urlLabel: 'Figma community page', muxID: 'x7qlhrctWFtNyvBGLMNqsWDY01cjQsgN85T00re004phyw'},
+    {name: 'Pixel Bloom', icon: pixelbloomIcon, muxID: 'WOAyqsQQ1tO02y8oZIrf7zPCXAuj6AfAdUI54GVuKXiA'},
 ];
 
 export const portfolios: Portfolio[] = [
-    { label: 'My first website', url: 'https://2017.ndrewgood.com', range: 'Nov. 2017 - July 2018', stack: 'Jekyll, Github Pages', icon: 'favicon_2017' },
-    { label: 'First React website', url: 'https://2018.ndrewgood.com', range: 'July 2018 - Sept. 2018', stack: 'Gatsby, Github Pages', icon: 'favicon_2018' },
-    { label: 'Freshman year portfolio', url: 'https://2019.ndrewgood.com', range: 'Sept. 2018 - Aug. 2019 ', stack: 'Vanilla, Github Pages', icon: 'favicon_2019' },
-    { label: 'Sophomore year portfolio', url: 'https://2020.ndrewgood.com', range: 'Aug. 2019 - July 2020', stack: 'Vanilla, Firebase', icon: 'favicon_2020' },
-    { label: 'Junior year portfolio', url: 'https://2021.ndrewgood.com', range: 'July 2020 - Dec. 2021', stack: 'Gatsby, Sanity, Netlify', icon: 'favicon_2021' },
-    { label: 'Post-grad archive site', url: 'https://2022.ndrewgood.com', range: 'Jan 2022 - Feb. 2025', stack: 'Gatsby, Netlify', icon: 'favicon_2022' },
+    { label: 'My first website', url: 'https://2017.ndrewgood.com', range: 'Nov. 2017 - July 2018', stack: 'Jekyll, Github Pages', icon: portfolio2017Icon },
+    { label: 'First React website', url: 'https://2018.ndrewgood.com', range: 'July 2018 - Sept. 2018', stack: 'Gatsby, Github Pages', icon: portfolio2018Icon },
+    { label: 'Freshman year portfolio', url: 'https://2019.ndrewgood.com', range: 'Sept. 2018 - Aug. 2019 ', stack: 'Vanilla, Github Pages', icon: portfolio2019Icon },
+    { label: 'Sophomore year portfolio', url: 'https://2020.ndrewgood.com', range: 'Aug. 2019 - July 2020', stack: 'Vanilla, Firebase', icon: portfolio2020Icon },
+    { label: 'Junior year portfolio', url: 'https://2021.ndrewgood.com', range: 'July 2020 - Dec. 2021', stack: 'Gatsby, Sanity, Netlify', icon: portfolio2021Icon },
+    { label: 'Post-grad archive site', url: 'https://2022.ndrewgood.com', range: 'Jan 2022 - Feb. 2025', stack: 'Gatsby, Netlify', icon: portfolio2022Icon },
 ];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import "@mux/mux-player"
+    import { onMount, onDestroy } from 'svelte';
 
 	import Icon from '$lib/components/Icon.svelte';
     import { portfolios, socials, projects } from '$lib/data';
@@ -26,6 +27,50 @@
         }
     }
 
+    // Projects
+
+    let activeProjectIndex = 0;
+    let currentDirection = 1;
+    let currentDuration = 0;
+    let player: any; // Temporary fix until we have proper types
+    let isPlaying = false; // Add this state variable
+    let isLoading = false;
+    let isTransitioning = false;
+
+    let currentTime = 0;
+    let timeUpdateInterval: ReturnType<typeof setInterval>;
+
+    let isHoveringOverVideo = false;
+    let mouseIsPressed = false;
+    let hoverIcon = 'pause';
+
+    onMount(() => {
+        timeUpdateInterval = setInterval(() => {
+            currentTime = player?.currentTime || 0;
+            if (currentTime > currentDuration - .2 && currentDuration > 10 && !isTransitioning) {
+                changeProjectVideo(1);
+            }
+        }, 100); // Updates 10 times per second for smoother display
+    });
+
+    onDestroy(() => {
+        if (timeUpdateInterval) {
+            clearInterval(timeUpdateInterval);
+        }
+    });
+
+    function changeProjectVideo(direction: number) {
+        currentDirection = direction;
+        isTransitioning = true;
+        setTimeout(() => {
+            activeProjectIndex = (activeProjectIndex + direction + projects.length) % projects.length;
+            setTimeout(() => {
+                isTransitioning = false;
+            }, 200);
+        }, 200);
+
+    }
+
     // Past portfolios
 
     let showPortfolios = false;
@@ -33,14 +78,22 @@
     let hoverAmount = 0;
     let activePortfolioIndex = 0;
 
-    let player: any; // Temporary fix until we have proper types
-    let isPlaying = false; // Add this state variable
+    // Mouse tracking
+    let mouseX = 0;
+    let mouseY = 0;
+
+    function handleMouseMove(event: MouseEvent) {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+    }
 </script>
 
-<header class="mt-24 flex justify-between">
+<svelte:window on:mousemove={handleMouseMove}/>
+
+<header class="md:mt-18 mt-8 flex justify-between">
     <div class="name">
-        <h1>Andrew Goodridge</h1>
-        <h3>@ndrewgood</h3>
+        <h2>Andrew Goodridge</h2>
+        <h4>@ndrewgood</h4>
     </div>
     <div 
         class="relative" 
@@ -76,58 +129,182 @@
         {#if showSocials}
         <div class="flex flex-row-reverse gap-1">
             {#each socials as social, index}
-                <a 
-                    href={social.url}
-                    target="_blank"
-                    class="icon-button"
-                    in:fly|global={{ duration: 300, y: 10, delay: 15 * index + 100 }}
-                    out:fly|global={{ duration: 300, y: -10, delay: 15 * index }}
-                >
-                    <Icon name={social.platform} />
-                </a>
+                {#if social.platform === 'email'}
+                    <button 
+                        class="icon-button"
+                        on:click={() => {
+                            navigator.clipboard.writeText('hey@ndrewgood.com');
+                        }}
+                        in:fly|global={{ duration: 300, y: 10, delay: 15 * index + 100 }}
+                        out:fly|global={{ duration: 300, y: -10, delay: 15 * index }}
+                    >
+                        <Icon name={social.platform} />
+                    </button>
+                {:else}
+                    <a 
+                        href={social.url}
+                        target="_blank"
+                        class="icon-button"
+                        in:fly|global={{ duration: 300, y: 10, delay: 15 * index + 100 }}
+                        out:fly|global={{ duration: 300, y: -10, delay: 15 * index }}
+                    >
+                        <Icon name={social.platform} />
+                    </a>
+                {/if}
             {/each}
         </div>
         {/if}
     </div>
 </header>
 
-<section class="mt-24">
-    <div class="top">
-        <div class="left">
-            <div class="image"></div>
-            <h1>Project Title</h1>
-        </div>
-        <div class="right">
-            <div class="progress"></div>
-        </div>
+<section class="mt-24 flex flex-col gap-4">
+    <div
+        class="fixed left-0 top-0 z-50 pointer-events-none -translate-x-1/2 -translate-y-1/2 drop-shadow-lg"
+        style:scale={mouseIsPressed ? 1.4 : 1.5}
+        style:opacity={isHoveringOverVideo ? '1' : '0'}
+        style:left={mouseX + 'px'}
+        style:top={mouseY + 'px'}
+        style:transition={'scale 0.1s ease-in-out'}
+    >
+        <Icon name={hoverIcon} color="--color-white" />
     </div>
-    <mux-player
-        bind:this={player}
-        role="button"
-        on:click={() => {
-            if (isPlaying) {
-                player.pause();
-            } else {
-                player.play();
-            }
-            isPlaying = !isPlaying;
-        }}
-        on:keydown={(e: KeyboardEvent) => {
-            if (e.key === 'Enter' || e.key === ' ') {
-                if (!isPlaying) {
-                    player.play();
-                    isPlaying = true;
-                }
-            }
-        }}
-        
-        playback-id={projects[0].muxID}
-        autoplay="true"
-        loop="true"
-        muted="true"
-        preload="auto"
-        tabindex="0"
-    ></mux-player>
+    <div class="flex justify-between items-center">
+        <div class="flex gap-3 items-center">
+            <div 
+                class="w-7 h-7 bg-surface-600 rounded-sm flex justify-center items-center"
+            >
+                <img 
+                    src={projects[activeProjectIndex].icon}
+                    alt={projects[activeProjectIndex].name} 
+                    style:opacity={isTransitioning || isLoading ? '0' : '1'}
+                    style:transition={'opacity 0.1s ease-in-out'}
+                    class="pointer-events-none select-none"
+                />
+            </div>
+            {#if !isTransitioning && !isLoading}
+                <h1
+                    in:fly={{ duration: 200, y: currentDirection === 1 ? 5 : -5, delay: 0 }}
+                    out:fly={{ duration: 200, y: currentDirection === 1 ? -5 : 5, opacity: 0 }}
+                    class="select-none"
+                >{projects[activeProjectIndex].name}</h1>
+                {#if projects[activeProjectIndex].urlLabel}
+                    <a 
+                        href={projects[activeProjectIndex].url} 
+                        target="_blank" 
+                        in:fade|global={{ duration: 100 }}
+                        out:fade|global={{ duration: 100 }}
+                        class="text-sm font-medium bg-surface-600 hover:bg-surface-700 transition-colors duration-150 px-3 py-1 rounded-full opacity-50"
+                    >
+                        {projects[activeProjectIndex].urlLabel}
+                    </a>
+                {/if}
+            {/if}
+        </div>
+            <div class="flex gap-2">
+                {#each projects as project, index}
+                    <div 
+                        class="h-1.5 rounded-full relative" 
+                        style:background-color={'var(--color-surface-600)'}
+                        style:width={index === activeProjectIndex ? '64px' : '6px'}
+                        style:transition={'width 0.3s ease-in-out'}
+                        style:transition-timing-function={'cubic-bezier(0.16, 1, 0.3, 1)'}
+                    >
+                        <div
+                            class="h-1.5 rounded-full absolute left-0 top-0"
+                            style:background-color={'var(--color-activeSymbol)'}
+                            style:opacity={index === activeProjectIndex ? '1' : '0'}
+                            style:box-shadow={index === activeProjectIndex ? 'var(--drop-shadow-activeSymbol)' : 'none'}
+                            style:width={`${currentTime / currentDuration * 100}%`}
+                            style:transition={'opacity 0.1s ease-in-out'}
+                        >
+                    </div>
+                </div>
+                {/each}
+            </div>
+    </div>
+    <div class="aspect-[4/3] w-full relative overflow-hidden rounded-xl">
+        <div 
+            class="absolute top-0 left-0 w-full h-full flex justify-center items-center z-10 cursor-none"
+            role="presentation"
+            on:mouseenter={() => isHoveringOverVideo = true}
+            on:mouseleave={() => isHoveringOverVideo = false}
+            on:mousedown={() => mouseIsPressed = true}
+            on:mouseup={() => mouseIsPressed = false}
+        >
+            <div 
+                class="w-1/5 h-full opacity-0"
+                role="button"
+                tabindex="0"
+                on:mouseenter={() => hoverIcon = "backwards"}
+                on:click={() => changeProjectVideo(-1)}
+                on:keydown={(e: KeyboardEvent) => {
+                    if (e.key === 'Enter' || e.key === ' ') {
+                        changeProjectVideo(-1)
+                    }
+                }}
+            ></div>
+            <div class="w-3/5 h-full opacity-0"
+                role="button"
+                tabindex="0"
+                on:mouseenter={() => hoverIcon = "pause"}
+                on:click={() => {
+                    if (isPlaying) {
+                        player.pause();
+                        hoverIcon = "play";
+                    } else {
+                        player.play();
+                        hoverIcon = "pause";
+                    }
+                    isPlaying = !isPlaying;
+                }}
+                on:keydown={(e: KeyboardEvent) => {
+                    if (e.key === 'Enter' || e.key === ' ') {
+                        if (!isPlaying) {
+                            player.play();
+                            isPlaying = true;
+                        }
+                    }
+                }}
+            ></div>
+            <div 
+                class="w-1/5 h-full opacity-0"
+                role="button"
+                tabindex="0"
+                on:mouseenter={() => hoverIcon = "forwards"}
+                on:click={() => changeProjectVideo(1)}
+                on:keydown={(e: KeyboardEvent) => {
+                    if (e.key === 'Enter' || e.key === ' ') {
+                        changeProjectVideo(1)
+                    }
+                }}
+            ></div>
+        </div>
+        <mux-player
+            bind:this={player}
+            on:loadstart={() => { isLoading = true }}
+            on:loadeddata={() => { 
+                isLoading = false; 
+                currentDuration = player.duration;
+                isPlaying = true;
+            }}
+            class="aspect-[4/3] rounded-xl overflow-hidden cursor-pointer"
+            class:opacity-0={isTransitioning || isLoading}
+            style:transform={isTransitioning || isLoading || !isPlaying ? 'scale(0.97)' : 'scale(1.01)'}
+            style:transition={'transform 0.3s ease-in-out, opacity 0.3s ease-in-out'}
+            style:transition-timing-function={'cubic-bezier(0.16, 1, 0.3, 1)'}
+            playback-id={projects[activeProjectIndex].muxID}
+            autoplay="true"
+            loop="true"
+            muted="true"
+            preload="auto"
+        ></mux-player>
+        <div 
+            class="absolute left-0 top-0 bg-surface-600 w-full -z-10 aspect-[4/3] rounded-xl"
+            style:transform={isTransitioning || isLoading || !isPlaying ? 'scale(0.97)' : 'scale(1)'}
+            style:transition={'transform 0.3s ease-in-out, opacity 0.3s ease-in-out'}
+            style:transition-timing-function={'cubic-bezier(0.16, 1, 0.3, 1)'}
+        ></div>
+    </div>
 </section>
 
 <section class="mt-24 flex w-full justify-center mb-32">
@@ -156,6 +333,7 @@
                 class="absolute left-0 w-full h-[74.71px] -z-10 rounded-xl bg-surface-550"
                 style="top: {activePortfolioIndex * 74.71}px;"
                 class:transition-all={hoverAmount > 1}
+                style:transition-timing-function={'cubic-bezier(0.16, 1, 0.3, 1)'}
                 in:fade={{ duration: 100 }}
                 out:fade={{ duration: 100 }}
             ></div>
@@ -168,7 +346,23 @@
                     in:fade|global={{ duration: 400, delay: 15 * i + 150 }}
                 >
                     <div class="flex flex-col justify-between">
-                        <h1>{portfolio.label}</h1>
+                        <div class="flex flex-row gap-1.5 transition-all duration-150">
+                            <div 
+                                class="transition-all duration-150 overflow-hidden"
+                                style:width={activePortfolioIndex === i && portfoliosHovered ? '20px' : '0px'}
+                            >
+                                {#if activePortfolioIndex === i && portfoliosHovered}
+                                    <img 
+                                        src={portfolio.icon} 
+                                        alt={portfolio.label} 
+                                        class="w-5 h-5 rounded-sm" 
+                                        in:fly={{ x: -10, duration: 150 }}
+                                        out:fly={{ x: -10, duration: 150 }}
+                                    />
+                                {/if}
+                            </div>
+                            <h2>{portfolio.label}</h2>
+                        </div>
                         <p class="opacity-50">{portfolio.range}</p>
                     </div>
                     <Icon name="arrow" className="opacity-50 group-hover:opacity-100 transition-opacity duration-150" />
